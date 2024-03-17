33d320a5edeeec23a533f3ed757c7e6ad7b9b3e9
santiago Garzon <79063950+maesedev@users.noreply.github.com>
2024-03-17 23:01:12 +0000

    feat Automatizar la ejecucion y guardar los datos en la carpeta sets

--- laboratorio corte 1/python/app.py
+++ laboratorio corte 1/python/app.py
@@ -1,31 +1,48 @@
 from merge_sort import merge_sort 
 from timeit import timeit
 import numpy as np
-
+from os import path 
+from datetime import datetime
 import pyperclip
 
 n = [1000,10000,100000,1000000,10000000]
 k = 50
+
+
 def gen_arr(n):
     return [ np.random.random() for _ in range(n) ]
 
+def format(float):
+    return str(float).replace(".",",")
 
 def main ():
     times = []
-    for i in range(k):
-        print(f"\nIteracion {i}")
-        arr = gen_arr(n [2] )
+    for cant_datos in n:
+        file_name = f"./sets/n_{cant_datos}_sets_{datetime.now()}.txt"  
+        with open(file_name,"a") as f:
+            f.write(f"n --> {cant_datos}\n")
+            for i in range(k):
+                
+                print(f"\nIteracion {i + 1}")
+                f.write(f"Iteracion {i + 1}\n")
 
-        # print("Array original:", arr)
+                arr = gen_arr(cant_datos )
 
-        time = timeit(lambda : merge_sort(arr), number = 300)
+                time = timeit(lambda : merge_sort(arr), number = 30)
 
-        # print("Array ordenado:", arr)
-        times.append(time)
-        print(time,"sec")
-    
-    mean_time = sum(times) / len(times)
-    print("mean Time: ", str(mean_time).replace(".",","))
+                # print("Array ordenado:", arr)
+                times.append(time)
+                f.write(f"iteration time: {format(time)} seconds\n")
+                f.write(f"mean accmulated time: {format(sum(times) / len(times))} seconds\n")
+                
+                print(time,"sec")
+            
+            
+            
+            mean_time = sum(times) / len(times)
+            print("mean Time: ", str(mean_time).replace(".",","))
+
+            f.write(f"mean Time for {cant_datos}: " +  str(mean_time).replace(".",",") + "\n\n")
 
 
 
