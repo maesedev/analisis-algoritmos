06bfdacfb0b0995e5ec891f55d5f4428d9d670d7
santiago Garzon <79063950+maesedev@users.noreply.github.com>
2024-03-17 03:21:30 +0000

    Feat aplicando el algoritmo

--- laboratorio corte 1/python/app.py
+++ laboratorio corte 1/python/app.py
@@ -1,1 +1,35 @@
-import merge_sort 
+from merge_sort import merge_sort 
+from timeit import timeit
+import numpy as np
+
+import pyperclip
+
+n = [1000,10000,100000,1000000,10000000]
+k = 50
+def gen_arr(n):
+    return [ np.random.randint(10000000) for i in range(n) ]
+
+
+def main ():
+    times = []
+    for i in range(k):
+        print(f"\nIteracion {i}")
+        arr = gen_arr(n [2] )
+
+        # print("Array original:", arr)
+
+        time = timeit(lambda : merge_sort(arr), number = 300)
+
+        # print("Array ordenado:", arr)
+        times.append(time)
+        print(time,"sec")
+    
+    mean_time = sum(times) / len(times)
+    print("mean Time: ", str(mean_time).replace(".",","))
+
+
+
+
+if __name__ == "__main__":
+    main()
+
